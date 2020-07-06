6a9927dcfa34b8f5c46ada9a3ddee219dd26adf6
Ewan Morrison <ewanmorrrison@gmail.com>
2020-07-06 11:10:36 +0100

    Add errors component to handle API errors

---
+++ src/app/global/Errors.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+import { Icon } from "./Icon";
+
+export const Errors: FunctionComponent<{ errors: string[] }> = ({ errors }) => {
+	if (typeof errors === "string") {
+		errors = [errors];
+	}
+	const formatError = (error: string) => {
+		return `${error[0].toUpperCase()}${error.substring(1)}${
+			error[error.length - 1] === "." ? "" : "."
+		}`;
+	};
+	return Array.isArray(errors) && errors.length > 0 ? (
+		<>
+			{errors.map((error) => (
+				<p key={error} className="paragraph paragraph--error">
+					<Icon withMargin="left">error</Icon>
+					{formatError(error)}
+				</p>
+			))}
+		</>
+	) : (
+		<></>
+	);
+};
