d41af556a67750c28f7ee29635c483341a0d51bc
Ewan Morrison <ewanmorrrison@gmail.com>
2020-07-25 23:34:36 +0100

    Reduce bundle size a little

--- src/app/workers/font-processing/convert-path-to-outline.ts
+++ src/app/workers/font-processing/convert-path-to-outline.ts
@@ -1,14 +1,14 @@
-import MakerJS from "makerjs";
+import { model, exporter, importer } from "makerjs";
 
 export function convertPathToOutline(svgPath: string): string {
-	const model = MakerJS.model.simplify(
-		MakerJS.importer.fromSVGPathData(svgPath, {
+	const makerObject = model.simplify(
+		importer.fromSVGPathData(svgPath, {
 			bezierAccuracy: 1,
 		})
 	);
 
-	return MakerJS.exporter
-		.toSVGPathData(MakerJS.model.outline(model, 5, 2, false), {
+	return exporter
+		.toSVGPathData(model.outline(makerObject, 5, 2, false), {
 			accuracy: 0,
 			fillRule: "nonzero",
 			origin: [0, 250],
