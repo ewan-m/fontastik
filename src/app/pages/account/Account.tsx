1586526a96263afdf64e6bf52aded2af2fe4c709
Ewan Morrison <ewanmorrrison@gmail.com>
2020-08-02 14:46:41 +0100

    Add editable profile sections

--- src/app/pages/account/Account.tsx
+++ src/app/pages/account/Account.tsx
@@ -1,15 +1,58 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Post } from "../home/Post";
-import "./Account.scss";
 import { Icon } from "../../global/Icon";
+import "./Account.scss";
+
+enum FormState {
+	initial = "initial",
+	editing = "editing",
+	sending = "sending",
+}
+
+const MiniForm: FunctionComponent<{ label: string; initialValue: string }> = ({
+	label,
+	initialValue,
+}) => {
+	const [formState, setFormState] = useState(FormState.initial);
+
+	switch (formState) {
+		case FormState.initial:
+			return (
+				<button
+					className="button  button__secondary button--large button--stacked"
+					onClick={() => {
+						setFormState(FormState.editing);
+					}}
+				>
+					{initialValue}
+					<Icon withMargin="right">edit</Icon>
+				</button>
+			);
+		case FormState.editing:
+			return (
+				<form className="form">
+					<label className="form__label">
+						{label}
+						<input className="form__input" value={initialValue}></input>
+					</label>
+					<div className="buttonRow">
+						<button className="button button__primary">Save changes</button>
+						<button className="button secondary">Cancel</button>
+					</div>
+				</form>
+			);
+		default:
+			return <>"this shouldn't happen :P"</>;
+	}
+};
 
 export const Account = () => {
 	const history = useHistory();
 	useEffect(() => {
 		// history.push("account/log-in");
 	}, []);
+
 	return (
 		<div className="accountPage">
 			<h2 className="pageTitle contentAppear">Your info.</h2>
@@ -26,12 +69,8 @@ export const Account = () => {
 					{/* <input type="file" className="button button__secondary"  style={{boxSizing: "border-box", width: "100%"} } accept="image/*"/> */}
 				</div>
 				<div className="profileInfo__fields">
-					<button className="button  button__secondary button--large button--stacked">
-						Sofia<Icon withMargin="right">edit</Icon>
-					</button>
-					<button className="button button__secondary button--large button--stacked">
-						itsemail@gmail.com<Icon withMargin="right">edit</Icon>
-					</button>
+					<MiniForm initialValue="Sofia" label="Name" />
+					<MiniForm initialValue="itsemail@gmail.com" label="Email" />
 					<button className="button button__primary button--stacked">
 						<Icon withMargin="left">lock_open</Icon>Change your password
 					</button>
@@ -40,10 +79,6 @@ export const Account = () => {
 					</button>
 				</div>
 			</div>
-			<h2 className="pageTitle contentAppear">Your posts.</h2>
-			<Post />
-			<Post />
-			<Post />
 		</div>
 	);
 };
