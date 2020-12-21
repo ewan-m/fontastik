f4ad4c383c6d0fad159e883bac5a770a85b31542
Ewan Morrison <ewanmorrrison@gmail.com>
2020-12-21 15:04:19 +0000

    Add test environment variables

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,3 +2,8 @@ export const environment = {
 	apiUrl: "https://fontastik-api.herokuapp.com/",
 	githubDataUrl: "https://cdn.statically.io/gh/ewan-m/fontastik-data/master",
 };
+
+const testEnvironment = {
+	apiUrl: "http://localhost:3000/",
+	githubDataUrl: "https://cdn.statically.io/gh/ewan-m/fontastik-data/test",
+};
