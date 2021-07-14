8f47306fdb9987abd1fd5266aa4c4d4f2c907037
Ewan Morrison <ewanmorrrison@gmail.com>
2021-07-14 16:37:08 +0100

    Fix linting for new prettier config

--- src/app/store/store-persistence-middleware.ts
+++ src/app/store/store-persistence-middleware.ts
@@ -5,22 +5,24 @@ interface AbstractStorage {
 	getItem: (key: string) => string | null;
 }
 
-export const persist = <T extends Record<string | number | symbol, unknown>>(
-	config: StateCreator<T>,
-	storage: AbstractStorage,
-	persistenceKey: string
-) => (set: SetState<T>, get: GetState<T>, api: StoreApi<T>): T => {
-	const state = config(
-		(payload) => {
-			set(payload);
-			storage.setItem(persistenceKey, JSON.stringify(get()));
-		},
-		get,
-		api
-	);
+export const persist =
+	<T extends Record<string | number | symbol, unknown>>(
+		config: StateCreator<T>,
+		storage: AbstractStorage,
+		persistenceKey: string
+	) =>
+	(set: SetState<T>, get: GetState<T>, api: StoreApi<T>): T => {
+		const state = config(
+			(payload) => {
+				set(payload);
+				storage.setItem(persistenceKey, JSON.stringify(get()));
+			},
+			get,
+			api
+		);
 
-	return {
-		...state,
-		...JSON.parse(storage.getItem(persistenceKey) ?? "{}"),
+		return {
+			...state,
+			...JSON.parse(storage.getItem(persistenceKey) ?? "{}"),
+		};
 	};
-};
