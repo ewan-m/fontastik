17a109e18e24ba8d6322d7f6cead0de6a8cdf2bd
Ewan Morrison <ewanmorrrison@gmail.com>
2021-09-06 10:01:00 +0100

    Store reference to worker so only 1 processing at once

--- src/app/font-processing/svg-font-string.ts
+++ src/app/font-processing/svg-font-string.ts
@@ -5,31 +5,39 @@ import {
 } from "../store/global-store";
 import type { Font } from "./font.type";
 
+let inProgressProcessing: Promise<svg2ttf.MicroBuffer> | null = null;
+
 export function convertToTTF(font: Font): Promise<svg2ttf.MicroBuffer> {
 	const updateProgress = useGenerationProgressStore.getState().updateProgress;
 
-	return new Promise((resolve, reject) => {
-		try {
-			const worker = new Worker(new URL("./font-worker.ts", import.meta.url), {
-				name: "font-worker",
-				type: "module",
-			});
-			worker.postMessage(font);
-			worker.onmessage = (e: {
-				data:
-					| { type: "result"; contents: svg2ttf.MicroBuffer }
-					| { type: "progress"; contents: GenerationProgress };
-			}) => {
-				if (e.data.type === "progress") {
-					updateProgress(
-						...(Object.values(e.data.contents) as [string, number, number])
-					);
-				} else if (e.data.type === "result") {
-					resolve(e.data.contents);
-				}
-			};
-		} catch (error) {
-			reject("Failed to create .ttf font");
-		}
-	});
+	if (inProgressProcessing === null) {
+		inProgressProcessing = new Promise((resolve, reject) => {
+			try {
+				const worker = new Worker(new URL("./font-worker.ts", import.meta.url), {
+					name: "font-worker",
+					type: "module",
+				});
+				worker.postMessage(font);
+				worker.onmessage = (e: {
+					data:
+						| { type: "result"; contents: svg2ttf.MicroBuffer }
+						| { type: "progress"; contents: GenerationProgress };
+				}) => {
+					if (e.data.type === "progress") {
+						updateProgress(
+							...(Object.values(e.data.contents) as [string, number, number])
+						);
+					} else if (e.data.type === "result") {
+						resolve(e.data.contents);
+						inProgressProcessing = null;
+					}
+				};
+			} catch (error) {
+				reject("Failed to create .ttf font");
+				inProgressProcessing = null;
+			}
+		});
+	}
+
+	return inProgressProcessing;
 }
