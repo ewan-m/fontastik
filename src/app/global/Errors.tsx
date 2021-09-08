e0379520b90d7e837096e2955e3a24d47fd112dc
Ewan Morrison <ewanmorrrison@gmail.com>
2021-09-08 17:27:59 +0100

    Create sentence case util, refactor Errors component

--- src/app/global/Errors.tsx
+++ src/app/global/Errors.tsx
@@ -1,26 +1,25 @@
 import * as React from "react";
 import type { FunctionComponent } from "react";
 import { Icon } from "./Icon";
+import { toSentenceCase } from "./toSentenceCase";
 
-export const Errors: FunctionComponent<{ errors: string[] }> = ({ errors }) => {
+export const Errors: FunctionComponent<{ errors: string[] | string }> = ({
+	errors,
+}) => {
 	if (typeof errors === "string") {
 		errors = [errors];
 	}
-	const formatError = (error: string) => {
-		return `${error[0].toUpperCase()}${error.substring(1)}${
-			error[error.length - 1] === "." ? "" : "."
-		}`;
-	};
-	return Array.isArray(errors) && errors.length > 0 ? (
+	const formatError = (error: string) =>
+		`${toSentenceCase(error)}${error[error.length - 1] === "." ? "" : "."}`;
+
+	return (
 		<>
-			{errors.map((error) => (
+			{errors?.map((error) => (
 				<p key={error} className="paragraph paragraph--error contentAppear">
 					<Icon withMargin="left">error</Icon>
 					{formatError(error)}
 				</p>
 			))}
 		</>
-	) : (
-		<></>
 	);
 };
---
+++ src/app/global/toSentenceCase.ts
@@ -0,0 +1,2 @@
+export const toSentenceCase = (input: string) =>
+	`${input[0].toUpperCase()}${input.substring(1)}`;
